6a3202ef5b2bb3d78ba1d81e5115fa1aa1be2ead
Rafael Mainieri <rafasolmainieri@gmail.com>
2024-06-19 11:46:32 -0300

    apresentando as informações do sorteio na tela

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let amigos = [];
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
-    amigos.push(amigos.value);
+    amigos.push(amigo.value);
 
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
@@ -16,6 +16,15 @@ function adicionar() {
 
 function sortear() {
     embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length -1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>'          
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>'
+        }
+    }
 }
 
 function embaralha(lista) {
